041dc6da7718c1585138112c7afa541e289169b1
Le Trung <trungle30902@gmail.com>
2023-04-11 17:33:02 +0700

    update: assets

---
+++ assets/assets.py
@@ -0,0 +1,11 @@
+IMAGE_FOLDOER_PATH='./assets/images'
+IMAE_DEFAULT_PATH= f'{IMAGE_FOLDOER_PATH}/image_default.jpg'
+
+FONT_FOLDER_PATH='./assets/fonts'
+FONT_POPPINS_LIGHT_PATH=f'{FONT_FOLDER_PATH}/poppins_light.ttf'
+FONT_POPPINS_REGULAR_PATH=f'{FONT_FOLDER_PATH}/poppins_regular.ttf'
+FONT_POPPINS_MEDIUM_PATH=f'{FONT_FOLDER_PATH}/poppins_medium.ttf'
+FONT_POPPINS_BOLD_PATH=f'{FONT_FOLDER_PATH}/poppins_bold.ttf'
+
+SOLUTION_FOLDER_PATH='./assets/solutions'
+SOLUTION_DEFAULT_PATH=f'{SOLUTION_FOLDER_PATH}/solution_default.json'
